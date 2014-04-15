9bd4c704a77ba85aefd4375e37c60e28bb6faacf
By Cristian Jaramillo <cristian_jaramillo@live.com.mx>
2014-04-14 23:57:50 -0500

    Add filter

--- 4filter.hs
+++ 4filter.hs
@@ -1,3 +1,9 @@
-filtro :: (a -> Bool) -> [a] -> [a]
-filtro _ [] = []
-filtro p (x:xs) | p x = x : filtro p xs | otherwise = filtro p xs
+-- Teniendo una lista [9,6,2,5,3,1], almacenar en otra cadena los elementos mayores a 5
+-- Utilizar: 'filter'
+filtro = filter (>5) [9,6,2,5,3,1]
+-- Se carga y se manda llamar el metodo 'filtro'
+-- Prelude> :load "4filter.hs"
+-- [1 of 1] Compiling Main             ( 4filter.hs, interpreted )
+-- Ok, modules loaded: Main.
+-- *Main> filtro
+-- [9,6]
